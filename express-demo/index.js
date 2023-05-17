a0aea2b6478316d13067094fc69b7a07b1c1f700
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-05-17 16:32:49 -0500

    1632-ports

--- express-demo/index.js
+++ express-demo/index.js
@@ -9,4 +9,6 @@ app.get('/', (req, res) => {
 app.get('/api/courses', (req, res) => {
 res.send([1,2,3,4]);
 });
-app.listen(3000, () => console.log('Listening on port 3000...'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
