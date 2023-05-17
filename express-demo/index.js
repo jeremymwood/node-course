6bd7b7b5336b3adc4cc2badb4d5d93b0b01f860c
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-05-17 16:29:03 -0500

    1628-nodemon

--- express-demo/index.js
+++ express-demo/index.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.send('Hello world!!!!!');
 });
 
 app.get('/api/courses', (req, res) => {
-res.send([1,2,3]);
+res.send([1,2,3,4]);
 });
 app.listen(3000, () => console.log('Listening on port 3000...'));
