6130a8574d3abaf7dc6ff4e89629c2e258df66dd
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-05-17 17:10:52 -0500

    1710-stuck

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,12 +2,36 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const courses = [
+    { id: 1, name: 'course 1' },
+    { id: 2, name: 'course 2' },
+    { id: 3, name: 'course 3' }
+];
+
 app.get('/', (req, res) => {
     res.send('Hello world!!!!!');
 });
 
 app.get('/api/courses', (req, res) => {
-res.send([1,2,3,4]);
+res.send(courses);
+});
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send(`Course not found`);
+    res.send(course);
+
 });
 
 const port = process.env.PORT || 3000;
