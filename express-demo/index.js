5874107fdc06d86d8436e8751a23d982c4019138
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-05-20 13:22:37 -0500

    1322-delete

--- express-demo/index.js
+++ express-demo/index.js
@@ -22,10 +22,7 @@ res.send(courses);
 
 app.post('/api/courses', (req, res) => {
     const { error } = validateCourse(req.body);
-    if (error) {
-        res.status(400).send(error.details[0].message);
-        return;
-    }
+    if (error) return res.status(400).send(error.details[0].message);
 
     const course = {
         id: courses.length + 1,
@@ -37,15 +34,22 @@ app.post('/api/courses', (req, res) => {
 
 app.put('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send(`Course not found`);
+    if (!course) return res.status(404).send(`Course not found`);
 
     const { error } = validateCourse(req.body);
-    if (error) {
-        res.status(400).send(error.details[0].message);
-        return;
-    }
+    if (error) return res.status(400).send(error.details[0].message);
 
     course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send(`Course not found`);
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
     res.send(course);
 });
 
@@ -59,7 +63,7 @@ function validateCourse(course) {
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send(`Course not found`);
+    if (!course) return res.status(404).send(`Course not found`);
     res.send(course);
 
 });
