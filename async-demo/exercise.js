801835b88a8a086c70843baf95242d09bb48cd31
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-05-31 11:56:11 -0500

    1156

---
+++ async-demo/exercise.js
@@ -0,0 +1,58 @@
+
+// getCustomer(1, (customer) => {
+//   console.log('Customer: ', customer);
+//   if (customer.isGold) {
+//     getTopMovies((movies) => {
+//       console.log('Top movies: ', movies);
+//       sendEmail(customer.email, movies, () => {
+//         console.log('Email sent...')
+//       });
+//     });
+//   }
+// });
+
+async function notifyCustomer() {
+  const customer = await getCustomer(1);
+  console.log('Customer: ', customer);
+  try {
+    if (customer.isGold) {
+      const movies = await getTopMovies();
+      console.log('Top movies: ', movies);
+      await sendEmail(customer.email, movies);
+      console.log('Email sent...')
+    }
+  }
+  catch (err) {
+    console.log('error: ', err.message);
+  }
+}
+notifyCustomer();
+
+function getCustomer(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        id: 1,
+        name: 'Mosh Hamedani',
+        isGold: true,
+        email: 'email'
+      });
+    }, 4000);
+  });
+}
+
+function getTopMovies(customer) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(['movie1', 'movie2']);
+    }, 4000);
+  });
+}
+
+function sendEmail(email, movies) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 4000);
+  });
+}
