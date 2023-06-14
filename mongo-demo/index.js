66a5dec577e13bd01c206b71a5b39c671b69d34d
Jeremy Wood <jeremy.mason.wood@gmail.com>
2023-06-14 14:23:39 -0500

    1423

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -45,4 +45,17 @@ async function getCourses() {
 
 }
 
-getCourses();
+// getCourses();
+
+
+async function updateCourse(id) {
+    const course = await Course.findById(id);
+    if (!course) return;
+
+    course.isPublished = true;
+    course.author = 'Another Author';
+
+    const result = await course.save()
+    console.log(result);
+}
+updateCourse('647e525eec634ae63c750cb7');
